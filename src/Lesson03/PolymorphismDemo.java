2f9ecab2bc4ca9e42ce1043defa1ee028d900f5b
Robert Gilanyi <bobgills51@gmail.com>
2022-09-02 10:42:20 -0600

    9/2/2022 updates

--- src/Lesson03/PolymorphismDemo.java
+++ src/Lesson03/PolymorphismDemo.java
@@ -1,20 +1,31 @@
-package Lesson03;
-
-import Lesson03.Liang.chpt11.*;
+package Lesson3.Liang.chpt11;
 
 public class PolymorphismDemo {
   /** Main method */
   public static void main(String[] args) {
     // Display circle and rectangle properties
-    displayObject(new CircleFromSimpleGeometricObject
+    displayObject(new Circle
         (1, "red", false));
-    displayObject(new RectangleFromSimpleGeometricObject
+    displayObject(new Rectangle
         (1, 1, "black", true));
+    
+    Object[] assortedObjs = {new Circle(1, "red", false), 
+        new Rectangle(1, 1, "black", true),
+        new Object[] {new Integer(3)}};
+    
+    displayObjects(assortedObjs);
   }
 
   /** Display geometric object properties */
-  public static void displayObject(SimpleGeometricObject object) {
+  public static void displayObject(GeometricObject object) {
     System.out.println("Created on " + object.getDateCreated() +
       ". Color is " + object.getColor());
+  }
+  
+    public static void displayObjects(Object[] objects) {
+        for (Object s : objects)
+            if(s instanceof GeometricObject)
+                System.out.println(s + " Created on " + ((GeometricObject)s).getDateCreated() +
+                ". Color is " + ((GeometricObject)s).getColor());
   }
 }
